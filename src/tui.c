c6261c5362157d1c5c15c550a75625df33892759
eliott.wnr <eliott.wanner@gmail.com>
2023-12-28 11:02:15 +0100

    set locale to system's default

--- src/tui.c
+++ src/tui.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <locale.h>
 
 void initCurses(){
+	setlocale(LC_ALL, ""); // use default system's locale
 	initscr(); 
 	cbreak(); 
 	keypad(stdscr, TRUE); 
